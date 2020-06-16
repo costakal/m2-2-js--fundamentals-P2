10f582dd5ecf570f3bfa65ceb20d0df4ce45ed2f
Costa Kalalafatidis <costa.kal@hotmail.com>
2020-06-16 15:27:29 -0400

    exercise 3

--- workshop/exercise-3.js
+++ workshop/exercise-3.js
@@ -5,9 +5,34 @@
 // Also, wouldn't it be nice if there were a Math method that could simplify the code....
 // https://www.w3schools.com/jsreF/jsref_obj_math.asp
 
+function max(num1, num2) {
+  if (num1 > num2) {
+    return num1;
+  } else {
+    return num2;
+  }
+}
+
+console.log(max(12, 42));
+
 // Q3.2
 // Without changing the function at all,
 // Can you figure out how we could use it to determine the greater number between 4 integers?
 
+function max(num1, num2) {
+  if (num1 > num2) {
+    return num1;
+  } else {
+    return num2;
+  }
+}
+
+console.log(max(max(12, 42), max(33, 99)));
+
 // Q3.3
 // Would this work with more integers?
+
+```
+yes but I would have to call the max function multiple times
+
+```;
