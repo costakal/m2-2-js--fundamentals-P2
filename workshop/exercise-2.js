376247121044b0b33b4125d581bdacab050a1ef0
Costa Kalalafatidis <costa.kal@hotmail.com>
2020-06-16 15:27:20 -0400

    exercise 2

--- workshop/exercise-2.js
+++ workshop/exercise-2.js
@@ -3,7 +3,13 @@
 // the greater one.
 
 // Define the function
-function max(num1, num2) {}
 
-// Call the function
-max(12, 43);
+function max(num1, num2) {
+  if (num1 > num2) {
+    console.log(num1);
+  } else {
+    console.log(num2);
+  }
+}
+
+max(12, 42);
